4ba7604dd1088a4f09115e7be8c53189ab974a3f
Anthony Chu <anthony@anthonychu.ca>
2022-04-27 21:44:16 -0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+require("dotenv").config();
+const axios = require("axios");
+const background = process.env.BACKGROUND_COLOR;
+
+const apiBaseUrl = process.env.API_BASE_URL || `https://album-api.${process.env.CONTAINER_APP_ENV_DNS_SUFFIX}`;
+const api = axios.create({
+  baseURL: apiBaseUrl,
+  params: {},
+  timeout: process.env.TIMEOUT || 15000,
+});
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  try {
+    console.log("Sending request to backend albums api");
+    var data = await api.get("/albums");
+    console.log("Response from backend albums api: ", data.data);
+    res.render("index", {
+      albums: data.data,
+      background_color: background,
+    });
+  } catch (err) {
+    console.log("Error: ", err);
+    next(err);
+  }
+});
+
+module.exports = router;
